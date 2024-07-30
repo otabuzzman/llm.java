915867cae9375b7ef885d3aec9974d42b6d2e01e
otabuzzman <iuergen.schuck@gmail.com>
2024-07-30 20:10:51 +0200

    Save work on Port

---
+++ src/com/otabuzzman/llmj/TestGpt2.java
@@ -0,0 +1,207 @@
+package com.otabuzzman.llmj;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+import java.rmi.UnexpectedException;
+
+public class TestGpt2 {
+
+    private FloatBuffer grads_memory;
+    private FloatBuffer expected_grads_memory;
+
+    // poor man's tensor checker
+    private boolean check_tensor(int a, int b, int n, String label) {
+        int print_upto = 5;
+        boolean ok = true;
+        float maxdiff = 0.0f;
+        float tol = 2e-2f;
+        System.out.println(label);
+        for (int i = 0; i < n; i++) {
+            // look at the diffence at position i of these two tensors
+            float diff = Math.abs(grads_memory.get(a + i) - expected_grads_memory.get(b + i));
+
+            // keep track of the overall error
+            ok = ok && (diff <= tol);
+            if (diff > maxdiff) { maxdiff = diff; }
+
+            // for the first few elements of each tensor, pretty print
+            // the actual numbers, so we can do a visual, qualitative proof/assessment
+            if (i < print_upto) {
+                if (diff <= tol) {
+                    if (i < print_upto) { System.out.print("OK "); }
+                } else {
+                    if (i < print_upto) { System.out.print("NOT OK "); }
+                }
+                System.out.printf("%f %f\n", grads_memory.get(a + i), expected_grads_memory.get(b + i));
+            }
+        }
+        // print the final result for this tensor
+        if (ok) {
+            System.out.printf("TENSOR OK, maxdiff = %e\n", maxdiff);
+        } else {
+            System.out.printf("TENSOR NOT OK, maxdiff = %e\n", maxdiff);
+        }
+        return ok;
+    }
+
+    public void run() throws FileNotFoundException, IOException, UnexpectedException {
+        // build the GPT-2 model from a checkpoint
+        GPT2 model = new GPT2("gpt2_124M.bin");
+
+        int C = model.config.channels;
+        int V = model.config.vocab_size;
+        int Vp = model.config.padded_vocab_size;
+        int maxT = model.config.max_seq_len;
+        int L = model.config.num_layers;
+
+        RandomAccessFile state_file = new RandomAccessFile("gpt2_124M_debug_state.bin", "r");
+        int[] state_header = new int[256];
+        for (int i = 0; i < 256; i++) {
+            state_header[i] = Integer.reverseBytes(state_file.readInt()); // convert little-endians in file to JVM big-endians
+        }
+        assert(state_header[0] == 20240327) : "Bad magic in model file (try `python train_gpt2.py`)";
+        assert(state_header[1] == 2) : "Wrong version in model file (try `python train_gpt2.py`)";
+
+        int B = state_header[2]; // batch size, e.g. 4
+        int T = state_header[3]; // time / sequence length (e.g. 64, up to maxT)
+        System.out.println("[State]");
+        System.out.println("batch_size: " + B);
+        System.out.println("seq_len: " + T);
+
+        ParameterTensors expected_grads = new ParameterTensors(model.config);
+        ByteBuffer expected_grads_buffer = ByteBuffer.allocate(model.num_parameters * 4 /*sizeof(float)*/);
+
+        // inputs and expected outputs, only used for error checking
+        ByteBuffer x_buffer = ByteBuffer.allocate(B * T * 4 /*sizeof(int)*/);
+        ByteBuffer y_buffer = ByteBuffer.allocate(B * T * 4 /*sizeof(int)*/);
+        ByteBuffer expected_logits_buffer = ByteBuffer.allocate(B * T * V * 4 /*sizeof(float)*/);
+        ByteBuffer expected_loss_buffer = ByteBuffer.allocate(1 * 4 /*sizeof(float)*/);
+        state_file.getChannel().read(x_buffer);
+        state_file.getChannel().read(y_buffer);
+        state_file.getChannel().read(expected_logits_buffer);
+        state_file.getChannel().read(expected_loss_buffer);
+        state_file.getChannel().read(expected_grads_buffer);
+        x_buffer.order(ByteOrder.LITTLE_ENDIAN);
+        x_buffer.flip(); // apply byte order
+        y_buffer.order(ByteOrder.LITTLE_ENDIAN);
+        y_buffer.flip(); // apply byte order
+        expected_logits_buffer.order(ByteOrder.LITTLE_ENDIAN);
+        expected_logits_buffer.flip(); // apply byte order
+        expected_loss_buffer.order(ByteOrder.LITTLE_ENDIAN);
+        expected_loss_buffer.flip(); // apply byte order
+        expected_grads_buffer.order(ByteOrder.LITTLE_ENDIAN);
+        expected_grads_buffer.flip(); // apply byte order
+        IntBuffer x = x_buffer.asIntBuffer();
+        IntBuffer y = y_buffer.asIntBuffer();
+        FloatBuffer expected_logits = expected_logits_buffer.asFloatBuffer();
+        FloatBuffer expected_loss = expected_loss_buffer.asFloatBuffer();
+        expected_grads_memory = expected_grads_buffer.asFloatBuffer();
+        state_file.close();
+
+        // overall OK signal for the test
+        boolean allok = true;
+
+        // let's do 10 training iterations, following the pytorch code
+        float expected_losses[] = {
+            5.270007133483887f,
+            4.059706687927246f,
+            3.3751230239868164f,
+            2.8007826805114746f,
+            2.315382242202759f,
+            1.8490285873413086f,
+            1.3946564197540283f,
+            0.9991465210914612f,
+            0.6240804195404053f,
+            0.37651097774505615f
+        };
+
+        for (int step = 0 ; step < 10 ; step++) {
+            long start = System.currentTimeMillis();
+
+            model.forward(x.array(), y.array(), B, T);
+            model.zero_grad();
+            model.backward();
+
+            long end = System.currentTimeMillis();
+
+            if (step == 0) {
+                // error checking at step 0 for reference activations/gradients
+                // at this point, target should be equal to expected_logits, let's compare
+                boolean logits_ok = true;
+                int calculated_logits = model.acts.logits;
+                float max_diff = 0.0f;
+                for (int bt = 0 ; bt < B * T ; bt++) {
+                    for (int v = 0 ; v < V ; v++) { // note we only loop to V (ignoring padding)
+                        int i = bt * Vp + v; // linearized index, using Vp
+                        if (i < 10) {
+                            System.out.printf("%f, %f\n", expected_logits.get(i), model.acts_memory.get(calculated_logits + i));
+                        }
+                        float diff = Math.abs(expected_logits.get(bt * V + v) - model.acts_memory.get(calculated_logits + i));
+                        max_diff = Math.max(max_diff, diff);
+                        if (diff >= 1e-2f) {
+                            System.out.printf("MISMATCH AT INDEX %d,%d: ", bt, v);
+                            System.out.printf("%f %f\n", expected_logits.get(bt * V + v), model.acts_memory.get(calculated_logits + i));
+                            logits_ok = false;
+                            bt = B*T; // to break out of both loops
+                            break;
+                        }
+                    }
+                }
+                if(!logits_ok) { System.out.printf("NOT "); }
+                System.out.printf("OK (LOGITS), max_diff = %e\n", max_diff);
+                allok = allok && logits_ok;
+
+                // compare the achieved loss
+                if (Math.abs(model.mean_loss - expected_loss.get(0)) >= 1e-2) {
+                    System.out.printf("LOSS MISMATCH: %f %f\n", model.mean_loss, expected_loss.get(0));
+                    allok = false;
+                } else {
+                    System.out.printf("LOSS OK: %f %f\n", model.mean_loss, expected_loss.get(0));
+                }
+
+                // finally check all the gradients
+                boolean[] gradoks = new boolean[16];
+                ParameterTensors grads = model.grads;
+                grads_memory = model.grads_memory;
+                gradoks[0] = check_tensor(grads.wte, expected_grads.wte, V * C, "dwte");
+                gradoks[1] = check_tensor(grads.wpe, expected_grads.wpe, maxT * C, "dwpe");
+                gradoks[2] = check_tensor(grads.ln1w, expected_grads.ln1w, L * C, "dln1w");
+                gradoks[3] = check_tensor(grads.ln1b, expected_grads.ln1b, L * C, "dln1b");
+                gradoks[4] = check_tensor(grads.qkvw, expected_grads.qkvw, L * 3 * C * C, "dqkvw");
+                gradoks[5] = check_tensor(grads.qkvb, expected_grads.qkvb, L * 3 * C, "dqkvb");
+                gradoks[6] = check_tensor(grads.attprojw, expected_grads.attprojw, L * C * C, "dattprojw");
+                gradoks[7] = check_tensor(grads.attprojb, expected_grads.attprojb, L * C, "dattprojb");
+                gradoks[8] = check_tensor(grads.ln2w, expected_grads.ln2w, L * C, "dln2w");
+                gradoks[9] = check_tensor(grads.ln2b, expected_grads.ln2b, L * C, "dln2b");
+                gradoks[10] = check_tensor(grads.fcw, expected_grads.fcw, L * 4 * C * C, "dfcw");
+                gradoks[11] = check_tensor(grads.fcb, expected_grads.fcb, L * 4 * C, "dfcb");
+                gradoks[12] = check_tensor(grads.fcprojw, expected_grads.fcprojw, L * C * 4 * C, "dfcprojw");
+                gradoks[13] = check_tensor(grads.fcprojb, expected_grads.fcprojb, L * C, "dfcprojb");
+                gradoks[14] = check_tensor(grads.lnfw, expected_grads.lnfw, C, "dlnfw");
+                gradoks[15] = check_tensor(grads.lnfb, expected_grads.lnfb, C, "dlnfb");
+                for (int i = 0 ; i < 16 ; i++) {
+                    allok = allok && gradoks[i];
+                }
+            }
+
+            model.update(1e-4f, 0.9f, 0.999f, 1e-8f, 0.01f, step + 1);
+
+            // compare the losses
+            float eXpected_loss = expected_losses[step];
+            float actual_loss = model.mean_loss;
+            boolean step_loss_ok = Math.abs(eXpected_loss - actual_loss) < 1e-2f;
+            allok = allok && step_loss_ok;
+
+            // print the timing information at the end
+            System.out.printf("step %d: loss %f (took %f ms) OK = %b\n", step, model.mean_loss, end - start, step_loss_ok);
+        }
+
+        // final judgement
+        System.out.printf("overall okay: %b\n", allok);
+    }
+}
---
+++ src/com/otabuzzman/llmj/TrainGpt2.java
@@ -0,0 +1,127 @@
+package com.otabuzzman.llmj;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.rmi.UnexpectedException;
+
+public class TrainGpt2 {
+
+    private long state = 1337;
+    private long random_u32() {
+        // xorshift rng: https://en.wikipedia.org/wiki/Xorshift#xorshift.2A
+        state ^= state >>> 12;
+        state ^= state << 25;
+        state ^= state >>> 27;
+        long u32 = (state * 0x2545F4914F6CDD1Dl) >>> 32;
+        return (u32 < 0) ? -u32 : u32;
+    }
+
+    private float random_f32() { // random float32 in [0,1)
+        return (random_u32() >>> 8) / 16777216.0f;
+    }
+
+    public void run() throws FileNotFoundException, IOException, UnexpectedException {
+        // build the GPT-2 model from a checkpoint
+        GPT2 model = new GPT2("gpt2_124M.bin");
+
+        // build the DataLoaders from tokens files. for now use tiny_shakespeare if available, else tiny_stories
+        String tiny_stories_train = "dev/data/tinystories/TinyStories_train.bin";
+        String tiny_stories_val = "dev/data/tinystories/TinyStories_val.bin";
+        String tiny_shakespeare_train = "dev/data/tinyshakespeare/tiny_shakespeare_train.bin";
+        String tiny_shakespeare_val = "dev/data/tinyshakespeare/tiny_shakespeare_val.bin";
+        String train_tokens = Files.exists(Paths.get(tiny_shakespeare_train)) ? tiny_shakespeare_train : tiny_stories_train;
+        String val_tokens = Files.exists(Paths.get(tiny_shakespeare_val)) ? tiny_shakespeare_val : tiny_stories_val;
+        int B = 4; // batch size 4 (i.e. 4 independent token sequences will be trained on)
+        int T = 64; // sequence length 64 (i.e. each sequence is 64 tokens long). must be <= maxT, which is 1024 for GPT-2
+        DataLoader train_loader = new DataLoader(train_tokens, B, T, 0, 1, true);
+        DataLoader val_loader = new DataLoader(val_tokens, B, T, 0, 1, false);
+        System.out.println("train dataset num_batches: " + train_loader.num_tokens / (B * T));
+        System.out.println("val dataset num_batches: " + val_loader.num_tokens / (B * T));
+        int val_num_batches = 5;
+
+        // build the Tokenizer
+        Tokenizer tokenizer = new Tokenizer("gpt2_tokenizer.bin");
+
+        // some memory for generating samples from the model
+        int[] gen_tokens = new int[B * T * 4 /*sizeof(int)*/];
+        int genT = 64; // number of steps of inference we will do
+
+        // train
+        long start, end;
+        for (int step = 0 ; step <= 40 ; step++) {
+
+            // once in a while estimate the validation loss
+            if (step % 10 == 0) {
+                float val_loss = 0.0f;
+                val_loader.reset();
+                for (int i = 0; i < val_num_batches; i++) {
+                    val_loader.next_batch();
+                    model.forward(val_loader.inputs, val_loader.targets, B, T);
+                    val_loss += model.mean_loss;
+                }
+                val_loss /= val_num_batches;
+                System.out.println("val loss " + val_loss);
+            }
+
+            // once in a while do model inference to print generated text
+            if (step > 0 && step % 20 == 0) {
+                // fill up gen_tokens with the GPT2_EOT, which kicks off the generation
+                for(int i = 0 ; i < B * T ; ++i) {
+                    gen_tokens[i] = tokenizer.eot_token;
+                }
+                // now sample from the model autoregressively
+                System.out.print("generating:\n---\n");
+                for (int t = 1 ; t < genT ; t++) {
+                    // note that inference is very wasteful here because for each token
+                    // we re-calculate the forward pass for all of (B,T) positions from scratch
+                    // but the inference here is just for sanity checking anyway
+                    // and we can maybe optimize a bit more later, with careful tests
+                    model.forward(gen_tokens, null, B, T);
+                    // furthermore, below we're only using b=0 (i.e. the first row) of all B rows
+                    // we're in principle running B "inference streams" in parallel here
+                    // but only using position 0
+                    // get the Vp-dimensional vector probs[0, t-1, :]
+                    int probs = model.acts.probs + (t - 1) * model.config.padded_vocab_size;
+                    float coin = random_f32();
+                    // note we're only sampling from the first V elements, ignoring padding
+                    // (the probabilities in the padded region should be zero anyway)
+                    int next_token;
+                    // sample index from probabilities (they must sum to 1!)
+                    // coin is a random number in [0, 1), usually from random_f32()
+                    float cdf = 0.0f;
+                    for (int i = 0; i < model.config.vocab_size; i++) {
+                        cdf += model.acts_memory.get(probs + i);
+                        if (coin < cdf) {
+                            next_token = i;
+                        }
+                    }
+                    next_token = model.config.vocab_size - 1; // in case of rounding errors
+
+                    gen_tokens[t] = next_token;
+                    // print the generated token, either using the Tokenizer or a fallback
+                    if (tokenizer.init_ok) {
+                        String token_str = tokenizer.decode(next_token);
+                        System.out.print(token_str);
+                    } else {
+                        // fall back to printing the token id
+                        System.out.print(next_token);
+                    }
+                    System.out.flush();
+                }
+                System.out.print("\n---\n");
+            }
+
+            // do a training step
+            start = System.currentTimeMillis();
+            train_loader.next_batch();
+            model.forward(train_loader.inputs, train_loader.targets, B, T);
+            model.zero_grad();
+            model.backward();
+            model.update(1e-4f, 0.9f, 0.999f, 1e-8f, 0.0f, step+1);
+            end = System.currentTimeMillis();
+            System.out.printf("step %d: train loss %f (took %f ms)\n", step, model.mean_loss, end - start);
+        }
+    }
+}
