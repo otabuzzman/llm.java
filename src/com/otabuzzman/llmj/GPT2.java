454d1ce1f2b76e88e5f2105fee933c65b755db3f
otabuzzman <iuergen.schuck@gmail.com>
2024-10-16 19:38:11 +0200

    Add non-TornadoVM attention functions to code

--- src/com/otabuzzman/llmj/GPT2.java
+++ src/com/otabuzzman/llmj/GPT2.java
@@ -386,7 +386,7 @@ public class GPT2 {
                             acts.set(out_bth + i, acts.get(out_bth + i) + att_btht2 * acts.get(value_t2 + i));
                         }
                     }
-                 }
+                }
             }
         }
     }
@@ -813,7 +813,7 @@ public class GPT2 {
         });
     }
 
-    public void attention_forward_1st(FloatArray handover, int out, int preatt, int att, int inp, int B, int T, int C, int NH) {
+    public void attention_forward_1st(FloatArray handover, int preatt, int inp, int B, int T, int C, int NH) {
         // input is (B, T, 3C) holding the query, key, value (Q, K, V) vectors
         // preatt, att are (B, NH, T, T). NH = number of heads, T = sequence length
         // that holds the pre-attention and post-attention scores (used in backward)
@@ -856,7 +856,7 @@ public class GPT2 {
         });
     }
 
-    public void attention_forward_2nd(FloatArray handover, int out, int preatt, int att, int inp, int B, int T, int C, int NH) {
+    public void attention_forward_2nd(FloatArray handover, int preatt, int att, int B, int T, int C, int NH) {
         // input is (B, T, 3C) holding the query, key, value (Q, K, V) vectors
         // preatt, att are (B, NH, T, T). NH = number of heads, T = sequence length
         // that holds the pre-attention and post-attention scores (used in backward)
@@ -888,7 +888,7 @@ public class GPT2 {
         });
     }
 
-    public void attention_forward_3rd(FloatArray handover, int out, int preatt, int att, int inp, int B, int T, int C, int NH) {
+    public void attention_forward_3rd(FloatArray handover, int out, int att, int inp, int B, int T, int C, int NH) {
         // input is (B, T, 3C) holding the query, key, value (Q, K, V) vectors
         // preatt, att are (B, NH, T, T). NH = number of heads, T = sequence length
         // that holds the pre-attention and post-attention scores (used in backward)
@@ -1168,7 +1168,7 @@ public class GPT2 {
         .transferToDevice(DataTransferMode.EVERY_EXECUTION, ind.tensors, acts_memory)
         .task("ln1", GPT2::layernorm_forward, params_memory, acts_memory, ind.tensors, ind.ln1, ind.ln1_mean, ind.ln1_rstd, ind.residual, ind.ln1w, ind.ln1b, B, T, C)
         .task("mm1", GPT2::matmul_forward, params_memory, acts_memory, ind.tensors, ind.qkv, ind.ln1, ind.qkvw, ind.qkvb, B, T, C, 3 * C)
-        .transferToHost(DataTransferMode.EVERY_EXECUTION, acts_memory, handover);
+        .transferToHost(DataTransferMode.EVERY_EXECUTION, acts_memory);
         TornadoExecutionPlan transformer_runner_1st = new TornadoExecutionPlan(transformer_block_1st.snapshot());
 
         TaskGraph transformer_block_2nd = new TaskGraph("tb2")
@@ -1177,7 +1177,7 @@ public class GPT2 {
         .task("at1", GPT2::attention_forward_1st, acts_memory, ind.tensors, handover, ind.preatt, ind.qkv, B, T, C, NH)
         .task("at2", GPT2::attention_forward_2nd, acts_memory, ind.tensors, handover, ind.preatt, ind.att, B, T, C, NH)
         .task("at3", GPT2::attention_forward_3rd, acts_memory, ind.tensors, handover, ind.atty, ind.att, ind.qkv, B, T, C, NH)
-        .transferToHost(DataTransferMode.EVERY_EXECUTION, acts_memory, handover);
+        .transferToHost(DataTransferMode.EVERY_EXECUTION, acts_memory);
         TornadoExecutionPlan transformer_runner_2nd = new TornadoExecutionPlan(transformer_block_2nd.snapshot());
 
         TaskGraph transformer_block_3rd = new TaskGraph("tb3")
@@ -1190,7 +1190,7 @@ public class GPT2 {
         .task("ge", GPT2::gelu_forward, acts_memory, ind.tensors, ind.fch_gelu, ind.fch, B * T * 4 * C)
         .task("mm4", GPT2::matmul_forward, params_memory, acts_memory, ind.tensors, ind.fcproj, ind.fch_gelu, ind.fcprojw, ind.fcprojb, B, T, 4 * C, C)
         .task("rs2", GPT2::residual_forward, acts_memory, ind.tensors, ind.residual3, ind.residual2, ind.fcproj, B * T * C)
-        .transferToHost(DataTransferMode.EVERY_EXECUTION, acts_memory, handover);
+        .transferToHost(DataTransferMode.EVERY_EXECUTION, acts_memory);
         TornadoExecutionPlan transformer_runner_3rd = new TornadoExecutionPlan(transformer_block_3rd.snapshot());
 
         // TaskGraph transformer_block = new TaskGraph("tb")
@@ -1219,10 +1219,19 @@ public class GPT2 {
             ind.updateForLayer(l);
             // now do the forward pass
             transformer_runner_1st.execute();
-            transformer_runner_2nd.execute();
+            // transformer_runner_2nd.execute();
+
+            int l_atty = ind.tensors.get(ind.atty);
+            int l_preatt = ind.tensors.get(ind.preatt);
+            int l_att = ind.tensors.get(ind.att);
+            int l_qkv = ind.tensors.get(ind.qkv);
+            // attention_forward(l_atty, l_preatt, l_att, l_qkv, B, T, C, NH);
+            attention_forward_1st(handover, l_preatt, l_qkv, B, T, C, NH);
+            attention_forward_2nd(handover, l_preatt, l_att, B, T, C, NH);
+            attention_forward_3rd(handover, l_atty, l_att, l_qkv, B, T, C, NH);
+
             transformer_runner_3rd.execute();
             // transformer_result = transformer_runner.execute();
-
         }
         // transformer_result.transferToHost(acts_memory);
         try {
